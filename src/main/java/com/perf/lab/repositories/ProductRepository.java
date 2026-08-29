54a49eba9062456d977577d5fdf2fb6858a5b94f
Dummy-Bug <laxyrajput@gmail.com>
2026-08-29 10:55:23 +0530

    connection to the DB success!

---
+++ src/main/java/com/perf/lab/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.perf.lab.repositories;
+
+import com.perf.lab.schema.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/perf/lab/schema/Product.java
@@ -0,0 +1,35 @@
+package com.perf.lab.schema;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String image;
+    private String category;
+
+    private String rating;
+
+}
